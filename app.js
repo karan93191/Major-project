96aca5bfa0551723e3b1324ed1d0f3d8bb30ce97
karan93191 <karangupta93191@gmail.com>
2025-09-27 16:44:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express=require("express")
 
 const app=express()
 const mongoose=require("mongoose")
-const Listing=require("../majorproject/models/listing")
+const Listing=require("./models/listing")
 const path=require("path")
 const methodOverride=require("method-override")
 const ejsMate=require("ejs-mate")
